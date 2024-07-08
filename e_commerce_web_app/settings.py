43275b87fbc1f7be75f74cd666e2e241d67b9a22
yasir <mohammadyasirkmt@gmail.com>
2024-07-08 15:40:28 +0530

    first commit

--- e_commerce_web_app/settings.py
+++ e_commerce_web_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a$qv20g+oo$vu+wk7&v!-xvt3%p3(08342vboqn+h@_goo89s=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['', '127.0.0.1']
+ALLOWED_HOSTS = ['shoppinglyx-efsn.onrender.com', '127.0.0.1']
 
 
 # Application definition
